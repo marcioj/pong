08f181cea0933ceea735709d271a5c174ccfd77a
Marcio Junior <marciojunior_eu@yahoo.com.br>
2020-04-30 10:23:19 +0800

    Make player go to center of screen if the ball is returning

--- com_player.js
+++ com_player.js
@@ -16,20 +16,35 @@ export default class COMPlayer extends BasePlayer {
       this.pongScreen.changeState("playing");
       return;
     } else if (this.pongScreen.state === "playing") {
-      // Without the roundUp, the COM this will have a shaking effect
-      const ballCenter = roundUpToNextMultiple(
-        this.pongScreen.ball.y - this.height / 2,
+      // Without the roundUp, the COM will have a shaking effect
+      const playerCenter = roundUpToNextMultiple(
+        this.y + this.height / 2,
         this.moveBy
       );
+      const ball = this.pongScreen.ball;
+      const ballCenter = roundUpToNextMultiple(ball.y, this.moveBy);
 
       // This determine how smart the COM is
       const doTheRightMove = rand(0, 1) === 1;
-
-      if (this.pongScreen.ball.xSpeed > 0 && doTheRightMove) {
-        if (ballCenter < this.y) {
-          this.moveUp();
-        } else if (ballCenter > this.y) {
+      // Follow the ball if it goes towards the player
+      if (ball.xSpeed > 0) {
+        if (doTheRightMove) {
+          if (playerCenter < ballCenter) {
+            this.moveDown();
+          } else if (playerCenter > ballCenter) {
+            this.moveUp();
+          }
+        }
+        // Otherwise return to the center
+      } else {
+        const screenCenter = roundUpToNextMultiple(
+          this.game.height / 2,
+          this.moveBy
+        );
+        if (playerCenter < screenCenter) {
           this.moveDown();
+        } else if (playerCenter > screenCenter) {
+          this.moveUp();
         }
       }
     }
